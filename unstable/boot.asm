217cfbaeb47aa2adc42fdd903d408372e5bc434d
KyleRS2004 <122418673+KyleRS2004@users.noreply.github.com>
2023-08-28 17:10:20 -0400

    Update boot.asm

--- unstable/boot.asm
+++ unstable/boot.asm
@@ -6,7 +6,7 @@ mov di, buffer		; sets di to the buffer
 mov ah, 03h
 mov al, 10		; ten sectors set to read
 mov ch, 1		; low eight bits of cylinder number
-mov cl, 7-16		; sectors two through eleven
+mov cl, 2-11		; sectors two through eleven
 mov dh, 1		; drive head 1
 mov dl, 1		; drive 1
 mov [es:bx], di
